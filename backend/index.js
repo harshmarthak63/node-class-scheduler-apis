c5c860e14670e53a606d4b3384421f6d7369401b
Harsh Marthak <harshmarthak63@gmail.com>
2026-02-12 18:23:51 +0530

    Initial MongoDB connection changes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
-const app =  express();
+const { default: mongoose } = require("mongoose");
+const app = express();
 
 app.get("/", (req, res) => {
-
+    
     res.json({
         message: "woho!!",
         success: true
-    })
+    });
 });
 
-app.listen(3000, () => {
-    console.log("Server running at port 3000");
+mongoose.connect("mongodb+srv://harshmarthak63_db_user:7bdhQFT4Fn7oanJa@cluster0.qbaqmyz.mongodb.net/?appName=Cluster0").then(() => {
+    console.log("DB connected");
+
+    app.listen(3000, () => {
+        console.log("Server running at port 3000");
+    });
+
+}).catch(err => {
+    console.log("DB not connected " + err);
 });
